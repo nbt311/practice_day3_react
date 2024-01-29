0a9b8412eaceb1d09e4871bab7bc84d014eade80
nbt311 <nbtrung0311@gmail.com>
2024-01-29 12:23:49 +0700

    Create AddCoponent.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Welcome from "./components/Welcome";
+import AddComponent from "./components/AddComponent";
 
 
 function App() {
   return (
   <div>
     <Welcome/>
+    <AddComponent firstNumber={1} secondNumber={2} />
   </div>
   );
 }
